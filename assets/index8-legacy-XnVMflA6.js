f8727a55159afe4968a3965e0cd8246c834a068c
lernult <loic.ernult@gmail.com>
2023-12-28 12:49:56 +0000

    Upgrade to last build

--- assets/index8-legacy-XnVMflA6.js
+++ assets/index8-legacy-XnVMflA6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-sBg-Vhft.js"],(function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
+System.register(["./index-legacy-vcKA07bL.js"],(function(o,e){"use strict";var t;return{setters:[o=>{t=o.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-8I_9VPyS.js
+++ assets/status-tap-legacy-8I_9VPyS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-sBg-Vhft.js","./index8-legacy-XaePMUSK.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
+System.register(["./index-legacy-vcKA07bL.js","./index8-legacy-XnVMflA6.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.r,r=e.a,s=e.w},e=>{i=e.f,o=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
