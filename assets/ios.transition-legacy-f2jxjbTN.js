061940c1545f1028806abdbadf2dd62c7a5bb982
lernult <loic.ernult@gmail.com>
2023-12-29 21:25:23 +0000

    Upgrade to last build

--- assets/ios.transition-legacy-f2jxjbTN.js
+++ assets/ios.transition-legacy-f2jxjbTN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-lRRO6ZLb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.j,n=t.k}],execute:function(){
+System.register(["./index-legacy-QmcExJqB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.j,n=t.k}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
