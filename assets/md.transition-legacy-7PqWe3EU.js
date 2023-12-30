afdf6a3f4d98b434f019d5f234f3ed8b3601d3ec
lernult <loic.ernult@gmail.com>
2023-12-30 22:37:22 +0000

    Upgrade to last build

--- assets/md.transition-legacy-7PqWe3EU.js
+++ assets/md.transition-legacy-7PqWe3EU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-VI0EKVoT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-gNDKfn84.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
