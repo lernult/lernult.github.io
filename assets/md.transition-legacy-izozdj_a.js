ac224a464bd27ae4c08b0f1d099d9713eb33652c
lernult <loic.ernult@gmail.com>
2023-12-30 21:53:25 +0000

    Upgrade to last build

--- assets/md.transition-legacy-izozdj_a.js
+++ assets/md.transition-legacy-izozdj_a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-y3Ws6XXQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-VI0EKVoT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-VTYcExTd.js
+++ assets/swipe-back-legacy-VTYcExTd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-y3Ws6XXQ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-VI0EKVoT.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
