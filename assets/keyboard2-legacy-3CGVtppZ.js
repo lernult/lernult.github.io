668f9bf9d0ff9a474179d8da87f6875bd0202328
lernult <loic.ernult@gmail.com>
2024-01-01 18:14:48 +0000

    Upgrade to last build

--- assets/keyboard2-legacy-3CGVtppZ.js
+++ assets/keyboard2-legacy-3CGVtppZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-gNDKfn84.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BGXoXbzJ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
